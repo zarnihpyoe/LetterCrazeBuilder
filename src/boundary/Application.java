969147faefe4655f772e1c2ba3abe096e4250771
Remy Kaldawy <remykaldawy@gmail.com>
2016-11-29 19:38:33 -0500

    added even more to boundary

--- src/boundary/Application.java
+++ src/boundary/Application.java
@@ -1,11 +1,29 @@
 package boundary;
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import model.Model;
+
 public class Application {
-	AbsBuilderGUI currentGUI;
+	JFrame frame;
+	Model m;
 	
-	public void initializeGUI(){
-		this.currentGUI.initializeViews();
-		this.currentGUI.initializeControllers();
+	
+	Application(Model m){
+		frame = new JFrame();
+		frame.setBounds(100, 100, 600, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.m = m;
 	}
+	
+	public void switchPanel (JPanel panel){
+		frame.setContentPane(panel);
+	}
+	
+	public void 
+	
 
 }
