dc0af80da704777a5af6f0440d15f6c5ef464056
Zarni Phyo <zarniphyo95@gmail.com>
2016-12-07 15:41:37 -0500

    feat: Complete build() in BoardBuilder

--- src/model/BoardBuilder.java
+++ src/model/BoardBuilder.java
@@ -16,12 +16,29 @@ public class BoardBuilder {
 		for(int i=0; i<6; i++) {
 			for(int j=0; j<6; j++) {
 				if(enabledTiles[i][j]) {
-					// TODO create new Tile and add it to the tiles
-					//tiles[i][j] = new Tile(i, j, );
+					tiles[i][j] = new Tile(i, j);
+				}
+				else {
+					tiles[i][j] = null;
 				}
 			}
 		}
+		tiles = linkTiles(tiles);
 		return new Board(tiles);
+	}
+	
+	private Tile[][] linkTiles(Tile[][] tiles) {
+		for(int j=0; j<6; j++) {
+			Tile pre = tiles[0][j];
+			for(int i=1; i<6; i++) {
+				if(tiles[i][j] != null) {
+					tiles[i][j].linkWith(pre);
+					pre = tiles[i][j];
+				}
+			}
+		}
+		
+		return tiles;
 	}
 	
 	
