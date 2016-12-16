a5e1c6dbcb3b666656384b8bee128849ce1397f0
Malika <alinurbe@gmail.com>
2016-12-15 19:34:02 -0500

    Last touch to Edit controllers, the board remembers selected tiles

--- src/controller/EditLightningController.java
+++ src/controller/EditLightningController.java
@@ -20,78 +20,103 @@ import model.ScoreBoardBuilder;
 
 public class EditLightningController extends MouseAdapter {
 	Application app;
-    Model model;
-    int lvl;
-    Scanner sc;
-	
+	Model model;
+	int lvl;
+	Scanner sc;
+
 	public EditLightningController(Application a, Model m, int lvl) {
 		this.app = a;
 		this.model = m;
 		this.lvl = lvl;
 	}
-	
+
 	@Override
 	public void mousePressed(MouseEvent me) {
-		
+		// Initiate new score board builder and board builder
+		ScoreBoardBuilder scoreBoard = new ScoreBoardBuilder();
+		BoardBuilder board = new BoardBuilder();
+
+		// Initiate new Lightning builder and lightning view
+		LightningBuilder lightBuilder = new LightningBuilder(board, scoreBoard);
+		LightningBuilderPanel lightView = new LightningBuilderPanel(this.model, this.app);
 
 		try {
 			StringFileIterator(new File (lvl+".txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("This file doesn't exist you fool.");
 		}
-		
+
 		String[][] levelBoard = new String[6][6];
-		
+
 		next();
-		
+
+		// The counter to keep track of number of the button that should be selected 
+		int j=0;
+
 		String row1 = next(); //000000
-		for(int i=0; i<6; i++){levelBoard[0][i] = Character.toString(row1.charAt(i));}
+		for(int i=0; i<6; i++, j++) { 
+			if (row1.charAt(i) == '0') {
+				lightView.setSelectedTiles(j);
+			}
+		}
 		String row2 = next(); //******
-		for(int i=0; i<6; i++){levelBoard[1][i] = Character.toString(row2.charAt(i));}
+		for(int i=0; i<6; i++, j++){
+			if (row2.charAt(i) == '0') {
+				lightView.setSelectedTiles(j);
+			}
+		}
 		String row3 = next();
-		for(int i=0; i<6; i++){levelBoard[2][i] = Character.toString(row3.charAt(i));}
+		for(int i=0; i<6; i++, j++){
+			if (row3.charAt(i) == '0') {
+				lightView.setSelectedTiles(j);
+			}
+		}
 		String row4 = next();
-		for(int i=0; i<6; i++){levelBoard[3][i] = Character.toString(row4.charAt(i));}
+		for(int i=0; i<6; i++, j++){
+			if (row4.charAt(i) == '0') {
+				lightView.setSelectedTiles(j);
+			}
+		}
 		String row5 = next();	
-		for(int i=0; i<6; i++){levelBoard[4][i] = Character.toString(row5.charAt(i));}
+		for(int i=0; i<6; i++, j++){
+			if (row5.charAt(i) == '0') {
+				lightView.setSelectedTiles(j);
+			}
+		}
 		String row6 = next();
-		for(int i=0; i<6; i++){levelBoard[5][i] = Character.toString(row6.charAt(i));}
-		
+		for(int i=0; i<6; i++, j++){
+			if (row6.charAt(i) == '0') {
+				lightView.setSelectedTiles(j);
+			}
+		}
+
 		int time = Integer.parseInt(next());
 		int thresh1 = Integer.parseInt(next());
 		int thresh2 = Integer.parseInt(next());
 		int thresh3 = Integer.parseInt(next());
-		
-		// Initiate new score board builder and board builder
-		ScoreBoardBuilder scoreBoard = new ScoreBoardBuilder();
-		BoardBuilder board = new BoardBuilder();
-		
-		// Initiate new Lightning builder and lightning view
-		LightningBuilder lightBuilder = new LightningBuilder(board, scoreBoard);
-		LightningBuilderPanel lightView = new LightningBuilderPanel(this.model, this.app);
-		
+
 		// Assign lightning builder status to model
 		this.model.assignBuilder(lightBuilder);
-		
+
 		// We want to call some method on current application that will switch to just created 
 		// puzzle view
 		app.modifyFrameSize(100, 100, 750, 575);
 		app.switchPanel(lightView);
-		
+
 		lightView.setLevelNumber(lvl);
 		lightView.setTimer(time);
 		lightView.setOneStar(thresh1);
 		lightView.setTwoStar(thresh2);
 		lightView.setThreeStar(thresh3);	
-		
+
 		lightView.validate();
-			
+
 	}
-	
+
 	public void StringFileIterator(File f) throws FileNotFoundException {
 		sc = new Scanner(f);
 	}
-	
+
 	public String next() throws NoSuchElementException {
 
 		if (sc == null) {
